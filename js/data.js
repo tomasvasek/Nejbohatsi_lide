5ea5273e67432ff481234bf2bab5e2e723f4f5a4
Tomáš Vašek <tomik.2001@seznam.cz>
2019-03-21 21:35:44 +0100

    druhy commit

---
+++ js/data.js
@@ -0,0 +1,68 @@
+var bohaci = [
+    {
+        "name":"Bill Gates",
+        "photo":"gates.jpg",   
+
+    },
+    {
+        "name":"Amancio Ortega",
+        "photo":"ortega.jpg",   
+    },
+    {
+        "name":"Jeff Bezos",
+        "photo":"bezos.jpg",   
+    },
+    {
+        "name":"Bernard Arnault",
+        "photo":"arnault.jpg",   
+    },
+    {
+        "name":"Warren Buffett",
+        "photo":"buffett.jpg",   
+    },            
+    {
+        "name":"Carlos Slim Helú",
+        "photo":"helu.jpg",   
+    },            
+    {
+        "name":"Larry Ellison",
+        "photo":"ellison.jpg",   
+    },            
+    {
+        "name":"Mark Zuckerberg",
+        "photo":"zuckerberg.jpg",   
+    },     
+    {
+        "name":"Larry Page",
+        "photo":"page.jpg",   
+    },        
+];
+var zajimavosti = [
+    {
+        "title":"Velké stěhování",
+        "text": "Newyorská burza byla založená v roce 1792, do ikonické a architektonicky ceněné budovy na Wall Street se ale přestěhovala až v roce 1903. Parket NYSE se dnes nachází v bezmála 24 metrů vysoké místnosti, jejíž strop je zdoben plátky zlata. Buttonwoodská dohoda, zakládající listina burzy, je k vidění v sedmém patře budovy v zaskleném boxu se stabilní teplotou a vlhkostí."
+    },
+    {
+        "title":"Dar od ruského cara",
+        "text": "Až do začátku 40. let 20. století zažívala Newyorská akciová burza paradoxně více emisí dluhových cenných papírů než úpisů akcií. V roce 1904 byly na NYSE ruským carem Mikulášem II. upsány dluhopisy v hodnotě jedné miliardy dolarů. Jako poděkování car burze věnoval obří vázu z dílny Fabergé, založené v roce 1842 ruským klenotníkem Carlem Fabergém. Archivář Newyorské akciové burzy Peter Asch říká, že jde dokonce o vůbec největší umělecký kousek vytvořený v dílně Fabergé."
+    },
+    {
+        "title":"Pablo Escobar a jeho kartel",
+        "text": "Pablo Escobar byl neuvěřitelně bohatý, tak bohatý, že to ani nelze vyčíslit. Ono je vůbec složité dělat účetnictví drogového krále a bosse nejmocnějšího drogového kartelu na světě. Možné je, že jeho bohatství přesáhlo 100 miliard dolarů, což by z něj dělalo nejbohatšího člověka planety."
+    },
+
+]
+var cesko = [
+    {
+        "name":"Andrej Babiš - 75 mld.KČ",
+        "photo": "babis.jpg"
+    },
+    {
+        "name":"Karel Komárek - 67 mld.KČ",
+        "photo": "komarek.jpg"
+    },
+    {
+        "name":"Radovan Vítek - 60 mld.KČ",
+        "photo": "vitek.jpg"
+    },
+];
--- js/main.js
+++ js/main.js
@@ -1,11 +1,56 @@
-$(function(){
+$(function () {
     $('#top p').hide();
-    $('#top h4').on('click', function(){
+    $('#top h4').on('click', function () {
         if ($(this).nextUntil('h4').is(':hidden')) {
-            $(this).css({'background-color':'#38a'});
+            $(this).css({ 'background-color': '#0014' });
         } else {
-            $(this).css({'background-color':'#444'});
+            $(this).css({ 'background-color': '#444' });
         }
         $(this).nextUntil('h4').toggle(500);
+    });
+
+    function zmenaTextu(i) {
+        $('#zajimavosti h4').text(zajimavosti[i].title);
+        $('#zajimavosti p').text(zajimavosti[i].text);
+    }
+
+    var a = 0;
+    zmenaTextu(a);
+    $('#zajimavosti .btn-success').on('click', function () {
+        a < zajimavosti.length - 1 ? a++ : a = 0;
+        zmenaTextu(a);
+    })
+
+    var i = 0;
+    window.setInterval(function () {
+        $('#cesko img').attr('src', 'img/' + cesko[i].photo);
+        $('#cesko figcaption').text(cesko[i].name);
+        i < cesko.length - 1 ? i++ : i = 0;
+    }, 3000);
+
+    var kviz = $('#kviz div.row');
+    bohaci.forEach(function (obj, idx) {
+        console.log(obj.photo);
+        kviz.append('<div class="col-sm-4"><figure id="' + idx + '"><img src="img/nic.jpg" alt="nic"><figcaption>'
+            + obj.name + '</figcaption></figure></div>');
+    });
+
+    var photo = $('#kviz img');
+    photo.on('click', function () {
+        var index = Math.floor(Math.random() * bohaci.length);
+        $(this).attr('src', 'img/' + bohaci[index].photo)
+            .attr('alt', bohaci[index].name);
+    });
+
+    var check = $('#kviz .btn-success').on('click', function () {
+        $('#kviz figure').each(function (idx, obj) {
+            var figcaption = $(obj).find('figcaption').text();
+            var alt = $(obj).find('img').attr('alt');
+            if (figcaption == alt) {
+                $(obj).find('img').css({ 'border': '2px solid green' });
+            } else {
+                $(obj).find('img').css({ 'border': '2px solid red' });
+            }
+        });
     });
 })
